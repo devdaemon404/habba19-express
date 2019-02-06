fd627b5175aeb9b354ff7619ab8a21f863eae859
windynik <nikhilprem@rocketmail.com>
2019-02-06 16:47:35 +0530

    Made a small change in the holympics validator

--- validators/holympicsValidator.js
+++ validators/holympicsValidator.js
@@ -1,18 +1,18 @@
 const joi = require('joi');
 
 const string = joi.string();
-
+const reqString= string.required();
 module.exports = {
     addingPoints: {
         body: {
-            points: string.required(),
-            name: string.required()
+            points: reqString,
+            name: reqString
         }
     },
     subtractingPoints: {
         body: {
-            points: string.required(),
-            name: string.required()
+            points: reqString,
+            name: reqString
         }
     }
 }
